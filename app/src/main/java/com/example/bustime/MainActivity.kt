ffec481db39647352713ed73a6061ebfffabf9a3
mkoki0822 <mkoki0422@gmail.com>
2020-04-27 18:28:02 +0900

    フォント

--- app/src/main/java/com/example/bustime/MainActivity.kt
+++ app/src/main/java/com/example/bustime/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-
+//git test
 class MainActivity : AppCompatActivity() {
 
 
